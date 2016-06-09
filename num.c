d5a0a8c802083ca0bdf6d440ddfa74f226e55d6e
Monisha0809 <monishasep95@gmail.com>
2016-06-09 22:30:30 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int i,sum=0;
+    for(i=0;i<=n;i++)
+    {
+    sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
+  return 0;
+}
