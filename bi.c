b88c6c7eea718ca07545cc3d5a2c1689a57b07a7
Monisha0809 <monishasep95@gmail.com>
2016-06-22 20:39:33 +0530

    Create bi.c

---
+++ bi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void bin(unsigned int n)
+{
+     unsigned int m=32768;
+     while(m>0)
+     {
+         if((n & m)==0)
+         printf("1");
+         else
+         printf("0");
+     }
+}
+int main()
+{
+     unsigned int n;
+     scanf("%u",n);
+     bin(n);
+     return 0;
+}
