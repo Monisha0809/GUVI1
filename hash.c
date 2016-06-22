0a984cfc971d9c986fbefa3c49abaec240adf20d
Monisha0809 <monishasep95@gmail.com>
2016-06-22 21:08:33 +0530

    Create hash.c

---
+++ hash.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],b[100]={0},i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+      b[a[i]]++;
+    }
+    for(i=0;i<n;i++)
+    {
+    	if(b[a[i]]==1)
+    	{
+    	printf("%d %d\n",a[i],b[a[i]]);
+    	b[a[i]]=0;
+    	break;
+        }
+    }
+ 	return 0;
+}
